f564304e228d908832e73bccd30aaf525fc74760
elHambreado <elquetienehambrecada5min@gmail.com>
2025-10-27 00:19:26 -0400

    Revert "Merge pull request #9 from heriiiiiii/feature/programming"
    This reverts commit f0d59fba01f92bba83b4636c353d425263883697, reversing
    changes made to 1b5aef7761ed8faf04a67b6fccf292ce3a8ec4f1.

--- Assets/01_Scripts/JQG_Scenes/JQG_LevelConnection.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName="Levels/Connection")]
-public class JQG_LevelConnection : ScriptableObject
-{
-   public static JQG_LevelConnection ActiveConnection {  get; set; }
-}
--- Assets/03_Art/NF_EffectAttack/NF_DestroyAfterAnimation.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class NF_DestroyAfterAnimation : MonoBehaviour
-{
-    void Start()
-    {
-        Destroy(gameObject, GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
-    }
-}
